c201467c3e33fc9f585910e255ed598dba0554fc
onkarsabale15 <onkarsabale15@gmail.com>
2023-02-25 20:51:06 +0530

    New user connection log on console is added

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 app.listen(PORT, ()=>{
     console.log("The Server Has Started\nListening on port :", PORT, `\nTo Access on local Machine ctrl + click on ==> http://localhost:${PORT}/`)
 });
 app.get('/', (req,res)=>{
+    console.log("A New User Just Connected")
     res.sendFile(__dirname + '/index.html')
 });
